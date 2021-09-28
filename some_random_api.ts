66fca6788fc4f9c57ffbbfc32676c892f587220f
Arcy <a@rqft.space>
2021-09-28 08:40:22 -0500

    homosexual

--- some_random_api.ts
+++ some_random_api.ts
@@ -14,10 +14,93 @@ export class SomeRandomAPI {
   async animal(animal: string) {
     return this.raw.getJSON<Animal>(`/animal/${animal}`);
   }
-  async animalDog() {
+  async animalFact(animal: string) {
+    return this.raw.getJSON<AnimalFact>(`/fact/${animal}`);
+  }
+  async animalImage(animal: string) {
+    return this.raw.getJSON<AnimalImage>(`/img/${animal}`);
+  }
+  async dog() {
     return this.animal("dog");
   }
-  async animalCat() {
+  async cat() {
     return this.animal("cat");
+  }
+  async panda() {
+    return this.animal("panda");
+  }
+  async fox() {
+    return this.animal("fox");
+  }
+  async redPanda() {
+    return this.animal("red_panda");
+  }
+  async koala() {
+    return this.animal("koala");
+  }
+  async bird() {
+    return this.animal("birb");
+  }
+  async raccoon() {
+    return this.animal("raccoon");
+  }
+  async kangaroo() {
+    return this.animal("kangaroo");
+  }
+
+  async dogFact() {
+    return this.animalFact("dog");
+  }
+  async catFact() {
+    return this.animalFact("cat");
+  }
+  async pandaFact() {
+    return this.animalFact("panda");
+  }
+  async foxFact() {
+    return this.animalFact("fox");
+  }
+  async redPandaFact() {
+    return this.animalFact("red_panda");
+  }
+  async koalaFact() {
+    return this.animalFact("koala");
+  }
+  async birdFact() {
+    return this.animalFact("birb");
+  }
+  async raccoonFact() {
+    return this.animalFact("raccoon");
+  }
+  async kangarooFact() {
+    return this.animalFact("kangaroo");
+  }
+
+  async dogImage() {
+    return this.animalImage("dog");
+  }
+  async catImage() {
+    return this.animalImage("cat");
+  }
+  async pandaImage() {
+    return this.animalImage("panda");
+  }
+  async foxImage() {
+    return this.animalImage("fox");
+  }
+  async redPandaImage() {
+    return this.animalImage("red_panda");
+  }
+  async koalaImage() {
+    return this.animalImage("koala");
+  }
+  async birdImage() {
+    return this.animalImage("birb");
+  }
+  async raccoonImage() {
+    return this.animalImage("raccoon");
+  }
+  async kangarooImage() {
+    return this.animalImage("kangaroo");
   }
 }
